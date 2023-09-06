fdf3d517d5809fe0dc44cb457977e9e2a9220f7b
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2023-09-06 13:06:03 +0200

    adds missing files to scripts

--- scripts/calculate_genotype_likelihoods.R
+++ scripts/calculate_genotype_likelihoods.R
@@ -1,11 +1,17 @@
-genotype_likelihood <- function(m,g,e,ref,alt){
-  (((m-g)*e+g*(1-e))^alt * ((m-g)*(1-e)+g*e)^ref)/(m^(ref+alt))
+genotype_likelihood <- function(m, g, e, ref, alt) {
+  (((m - g) * e + g * (1 - e))^alt * ((m - g) * (1 - e) + g * e)^ref) / (m^(ref + alt))
 }
 
-# For g = 0 (i.e. 0 reference alleles)
--10*log10(genotype_likelihood(m = 2, g= 0, e = 0.01, ref = 22, alt = 4))
-# [1] 80.96026 
--10*log10(genotype_likelihood(m = 2, g= 1, e = 0.01, ref = 22, alt = 4))
-# [1] 78.2678
--10*log10(genotype_likelihood(m = 2, g= 2, e = 0.01, ref = 22, alt = 4))
-# [1] 440.1746
+genotype_likelihood(m = 2, g = 1, e = 0.01, ref = 22, alt = 4)
+
+gl <- sapply(c(0, 1, 2), function(x) {
+  genotype_likelihood(
+    m = 2,
+    g = x,
+    e = 0.01,
+    ref = 22,
+    alt = 4
+  )
+})
+
+-10 * log10(gl)
