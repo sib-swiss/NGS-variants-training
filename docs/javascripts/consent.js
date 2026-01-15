3825c7f296bbe631bdc59ca0c69be07354e82126
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2026-01-15 11:51:36 +0100

    adds cookie banner and tracking

--- docs/javascripts/consent.js
+++ docs/javascripts/consent.js
@@ -1,16 +1,15 @@
-
-<!-- Matomo -->
-<script>
+document$.subscribe(function() {
+  var consent = __md_get("__consent")
+  if (consent && consent.matomo) {
     var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-      var u="{{ config.extra.analytics.base_url }}";
+      var u="https://matomo.sib.swiss/";
       _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '{{ config.extra.analytics.site_id }}']);
+      _paq.push(['setSiteId', '220']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
     })();
-  </script>
-  <!-- End Matomo Code -->
+  }
+})
